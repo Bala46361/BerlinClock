b613e75a1cb2d2df80fd0a2cb5eb7964fee82882
Balasubramanian <bala_465@rediffmail.com>
2020-05-26 17:36:43 +0530

    Refactored: Extracted few more constants in solution class for better readability

--- src/main/java/com/kata/berlin/BerlinClock.java
+++ src/main/java/com/kata/berlin/BerlinClock.java
@@ -2,11 +2,13 @@ package com.kata.berlin;
 
 public class BerlinClock {
 
+	private static final String O = "O";
+	private static final String R = "R";
 	private static final int TOTAL_LAMPS_IN_FIVE_HOURS_ROW = 4;
 	private static final int FIVE = 5;
 	private static final int ZERO = 0;
 	private static final int TWO = 2;
-	private static final String OFF = "O";
+	private static final String OFF = O;
 	private static final String ON = "Y";
 
 	public String getSeconds(int seconds) {
@@ -18,9 +20,9 @@ public class BerlinClock {
 		StringBuilder result = new StringBuilder();
 		for (int index = 0; index < TOTAL_LAMPS_IN_FIVE_HOURS_ROW; index++)
 			if (index < onLampsCount)
-				result.append("R");
+				result.append(R);
 			else
-				result.append("O");
+				result.append(O);
 		return result.toString();
 	}
 
