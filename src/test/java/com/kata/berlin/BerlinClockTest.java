a40e95fd8a7da2cdcd437149a0fe7805c6f05963
Balasubramanian <bala_465@rediffmail.com>
2020-05-26 15:46:29 +0530

    Refactored: Extracted instance initialization in test class for reusability

--- src/test/java/com/kata/berlin/BerlinClockTest.java
+++ src/test/java/com/kata/berlin/BerlinClockTest.java
@@ -3,20 +3,25 @@ package com.kata.berlin;
 import static org.junit.Assert.assertThat;
 
 import org.hamcrest.CoreMatchers;
+import org.junit.Before;
 import org.junit.Test;
 
 public class BerlinClockTest {
+	BerlinClock berlinClock;
+
+	@Before
+	public void initialize() {
+		berlinClock = new BerlinClock();
+	}
 
 	@Test
 	public void secondLightShouldTurnOnForEvenSeconds() {
-		BerlinClock berlinClock = new BerlinClock();
 
 		assertThat(berlinClock.getSeconds(22), CoreMatchers.is("Y"));
 	}
 
 	@Test
 	public void secondLightShouldTurnOffForOddSeconds() {
-		BerlinClock berlinClock = new BerlinClock();
 
 		assertThat(berlinClock.getSeconds(21), CoreMatchers.is("O"));
 	}
