cc81dca68cc0d6a80f8e4d3d44a1560f70b1df21
Balasubramanian <bala_465@rediffmail.com>
2020-05-26 17:35:46 +0530

    Refactored: Extracted constants, used stringbuilder and renamed constants to make it more meaningful and readable

--- src/main/java/com/kata/berlin/BerlinClock.java
+++ src/main/java/com/kata/berlin/BerlinClock.java
@@ -2,6 +2,8 @@ package com.kata.berlin;
 
 public class BerlinClock {
 
+	private static final int TOTAL_LAMPS_IN_FIVE_HOURS_ROW = 4;
+	private static final int FIVE = 5;
 	private static final int ZERO = 0;
 	private static final int TWO = 2;
 	private static final String OFF = "O";
@@ -12,14 +14,14 @@ public class BerlinClock {
 	}
 
 	public String convertHoursToFiveHoursRow(int hours) {
-		int onLampsCount = hours / 5;
-		String result = "";
-		for (int i = 0; i < 4; i++)
-			if (i < onLampsCount)
-				result += "R";
+		int onLampsCount = hours / FIVE;
+		StringBuilder result = new StringBuilder();
+		for (int index = 0; index < TOTAL_LAMPS_IN_FIVE_HOURS_ROW; index++)
+			if (index < onLampsCount)
+				result.append("R");
 			else
-				result += "O";
-		return result;
+				result.append("O");
+		return result.toString();
 	}
 
 }
