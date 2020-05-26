e475f6a10ace77b08d21cf2d6aa0088d1febfe77
Balasubramanian <bala_465@rediffmail.com>
2020-05-26 15:47:46 +0530

    Refactored: Modified the return statement for better readability

--- src/main/java/com/kata/berlin/BerlinClock.java
+++ src/main/java/com/kata/berlin/BerlinClock.java
@@ -3,12 +3,7 @@ package com.kata.berlin;
 public class BerlinClock {
 
 	public String getSeconds(int seconds) {
-
-		if (seconds % 2 == 0) {
-			return "Y";
-		} else {
-			return "O";
-		}
+		return seconds % 2 == 0 ? "Y" : "O";
 	}
 
 }
