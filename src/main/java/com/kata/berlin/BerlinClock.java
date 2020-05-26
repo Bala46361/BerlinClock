81427225fef932da045dce423b8badda5c1e7424
Balasubramanian <bala_465@rediffmail.com>
2020-05-26 15:49:22 +0530

    Refactored: Extracted constants in solution class for better readability

--- src/main/java/com/kata/berlin/BerlinClock.java
+++ src/main/java/com/kata/berlin/BerlinClock.java
@@ -2,8 +2,13 @@ package com.kata.berlin;
 
 public class BerlinClock {
 
+	private static final int ZERO = 0;
+	private static final int TWO = 2;
+	private static final String OFF = "O";
+	private static final String ON = "Y";
+
 	public String getSeconds(int seconds) {
-		return seconds % 2 == 0 ? "Y" : "O";
+		return seconds % TWO == ZERO ? ON : OFF;
 	}
 
 }
